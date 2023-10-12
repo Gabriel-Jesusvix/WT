73b1fac922813954acd56b6e641da4613274b47d
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-10-12 01:53:45 -0300

    Finish create SignUp screen

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,60 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import BackgroundIMG from '@assets/background.png'
+import Logo from '@assets/logo.svg'
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
+
+export function SignUp() {
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1}}showsVerticalScrollIndicator={false}>
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image
+          source={BackgroundIMG}
+          alt='Pessoas treinando'
+          resizeMode='contain'
+          position="absolute"
+        />
+        <Center my={24}>
+          <Logo />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Crie sua conta
+          </Heading>
+
+          <Input
+            placeholder='Nome'
+          />
+          <Input
+            placeholder='Email'
+            keyboardType='email-address'
+            autoCapitalize='none'
+          />
+          <Input
+            placeholder='Senha'
+            secureTextEntry
+          />
+          <Input
+            placeholder='Confirme sua senha'
+            secureTextEntry
+          />
+
+          <Button
+            title='Criar e acessar'
+          />
+        </Center>
+
+       
+          
+          <Button
+            title='Voltar para o login'
+            variant="outline"
+            mt={24}
+          />
+      </VStack>
+    </ScrollView>
+  )
+}
