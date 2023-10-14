fc0bb129c9d249dcb29131ef86014162f8ed385a
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-10-14 18:45:05 -0300

    Implements skeleton in avatar user profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,44 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserAvatar } from "@components/UserAvatar";
+import { Center, Skeleton, VStack } from "native-base";
+import React, { useState } from "react";
+import { ScrollView } from "react-native";
 
-
+const PHOTO_SIZE = 33
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Profile" />
+      <ScrollView>
+        <Center
+          mt={6}
+          px={10}
+        >
+          {
+            photoIsLoading ? (
+              <Skeleton
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.500"
+                endColor="gray.400"
+              />
+            ) : (
+              <UserAvatar
+                source={{ uri: 'https://github.com/Gabriel-Jesusvix.png' }}
+                alt="Imagem do usuario"
+                size={PHOTO_SIZE}
+              />
+            )
+
+          }
+
+
+        </Center>
+
+      </ScrollView>
+    </VStack>
   )
 }
