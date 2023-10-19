5b39b2e2504e6a1e506c189af0c42093d43c0a1a
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-10-19 10:58:03 -0300

    Create api service and interceptors.

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,7 +4,9 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigation } from "@react-navigation/native";
-import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
+import { Center, Heading, Image, ScrollView, Text, VStack, useToast } from "native-base";
 import { Controller, useForm } from 'react-hook-form';
 import * as yup from "yup";
 
@@ -38,13 +40,30 @@ export function SignUp() {
       password_confirm: ''
     },
   });
+  const toast = useToast()
 
   function handleGoBackToSignIn() {
     goBack()
   }
 
-  async function handleSignUp(data: FormDataProps) {
-    console.log(data)
+  async function handleSignUp({name, email,password,password_confirm}: FormDataProps) {
+    try {
+      const response = await api.post('/users' , {
+        name, email,password,password_confirm
+      })
+      console.log(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde'
+      if(isAppError) {
+        toast.show({
+          title,
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      }
+    }
+
   }
 
   return (
