aef06f5beaaa9805a6f523ecb4f644ee06819a47
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-10-20 14:29:52 -0300

    adjust loading in screen exercise

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -2,6 +2,7 @@ import BodySVG from '@assets/body.svg';
 import RepetitionsSVG from '@assets/repetitions.svg';
 import SeriesSVG from '@assets/series.svg';
 import { Button } from '@components/Button';
+import { Loading } from '@components/Loading';
 import { ExerciseDTO } from '@dtos/exerciseDTO';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -17,6 +18,7 @@ type RouteParams = {
 }
 export function Exercise() {
   const { goBack } = useNavigation<AppNavigatorBottomRoutesProps>()
+  const [isLoading, setIsLoading] = useState(true);
   const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO);
   const route = useRoute()
   const { exerciseId } = route.params as RouteParams
@@ -28,6 +30,7 @@ export function Exercise() {
   }
   async function fetchExerciseDetails() {
     try {
+      setIsLoading(true)
       const response = await api.get(`/exercises/${exerciseId}`);
 
       setExercise(response.data);
@@ -41,95 +44,104 @@ export function Exercise() {
         placement: 'top',
         bgColor: 'red.500'
       })
+    } finally {
+      setIsLoading(false)
     }
   }
 
   useEffect(() => {
     fetchExerciseDetails();
-  },[exerciseId])
+  }, [exerciseId])
 
   return (
     <VStack flex={1}>
-        <VStack
-          px={8}
-          bg="gray.600"
-          pt={12}
+      <VStack
+        px={8}
+        bg="gray.600"
+        pt={12}
+      >
+        <TouchableOpacity onPress={handleGoBackScreen}>
+          <Icon
+            as={Feather}
+            name="arrow-left"
+            color="green.500"
+            size={6}
+          />
+        </TouchableOpacity>
+        <HStack
+          justifyContent="space-between"
+          mt={4}
+          mb={8}
+          alignItems="center"
         >
-          <TouchableOpacity onPress={handleGoBackScreen}>
-            <Icon
-              as={Feather}
-              name="arrow-left"
-              color="green.500"
-              size={6}
-            />
-          </TouchableOpacity>
+          <Heading
+            color="gray.100"
+            fontSize="lg"
+            flexShrink={1}
+          >
+            {exercise.name}
+          </Heading>
+
           <HStack
-            justifyContent="space-between"
-            mt={4}
-            mb={8}
             alignItems="center"
           >
-            <Heading
-              color="gray.100"
+            <BodySVG />
+            <Text
+              color="gray.200"
+              ml={1}
               fontSize="lg"
-              flexShrink={1}
+              textTransform="capitalize"
             >
-              {exercise.name}
-            </Heading>
-
-            <HStack
-              alignItems="center"
-            >
-              <BodySVG />
-              <Text
-                color="gray.200"
-                ml={1}
-                fontSize="lg"
-                textTransform="capitalize"
-              >
-                {exercise.group}
-              </Text>
-            </HStack>
+              {exercise.group}
+            </Text>
           </HStack>
-        </VStack>
-        <ScrollView>
-        <VStack p={8}>
-        <Box rounded="lg" mb={3} overflow="hidden">
-          <Image
-            w="full"
-            h={80}
-            source={{ uri: `${api.defaults.baseURL}/exercise/demo/${exercise?.demo}` }}
-            alt="Nome do exercício"
-            resizeMode="cover"
-            rounded="lg"
-          />
-        </Box>
-          <Box bg="gray.600" rounded="md" pb={4} px={4}>
-            <HStack
-              alignItems="center"
-              justifyContent="space-around"
-              mb={6}
-              mt={5}
-            >
-              <HStack>
-                <SeriesSVG />
-                <Text color="gray.200" ml={2}>
-                {exercise.series} séries
-                </Text>
-              </HStack>
-              <HStack>
-                <RepetitionsSVG />
-                <Text color="gray.200" ml="2">
-                {exercise.repetitions} repetições
-                </Text>
-              </HStack>
-            </HStack>
-            <Button
-              title='Marcar como realizado'
-            />
-          </Box>
-        </VStack>
-      </ScrollView>
+        </HStack>
+      </VStack>
+      {
+        isLoading ? (
+          <Loading />
+        ) : (
+          <ScrollView>
+            <VStack p={8}>
+              <Box rounded="lg" mb={3} overflow="hidden">
+                <Image
+                  w="full"
+                  h={80}
+                  source={{ uri: `${api.defaults.baseURL}/exercise/demo/${exercise?.demo}` }}
+                  alt="Nome do exercício"
+                  resizeMode="cover"
+                  rounded="lg"
+                />
+              </Box>
+              <Box bg="gray.600" rounded="md" pb={4} px={4}>
+                <HStack
+                  alignItems="center"
+                  justifyContent="space-around"
+                  mb={6}
+                  mt={5}
+                >
+                  <HStack>
+                    <SeriesSVG />
+                    <Text color="gray.200" ml={2}>
+                      {exercise.series} séries
+                    </Text>
+                  </HStack>
+                  <HStack>
+                    <RepetitionsSVG />
+                    <Text color="gray.200" ml="2">
+                      {exercise.repetitions} repetições
+                    </Text>
+                  </HStack>
+                </HStack>
+                <Button
+                  title='Marcar como realizado'
+                />
+              </Box>
+            </VStack>
+          </ScrollView>
+        )
+      }
+
     </VStack>
   )
 }
